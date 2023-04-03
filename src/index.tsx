fd109795ad6b8fe5378a59f3d3647ffa79566431
Apple <apple@Apples-MacBook-Pro.local>
2023-04-03 14:01:39 +0530

    working

--- src/index.tsx
+++ src/index.tsx
@@ -31,7 +31,7 @@ setupAxios(axios)
 Chart.register(...registerables)
 
 const queryClient = new QueryClient()
-const container = document.getElementById('root')
+const container = document.getElementById('root2')
 if (container) {
   createRoot(container).render(
     <QueryClientProvider client={queryClient}>
